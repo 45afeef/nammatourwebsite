fc8fe0ac3a4be44770a6cbb69a3cb4bc60a4efa0
Afeef K K <45afeef@gmail.com>
2025-08-16 20:15:44 +0530

    Added ms clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,6 +18,22 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Raqlin | Move for a Purpose",
   description: "Budget Friendly Tour Packages For Indian Middle Class ",
+  openGraph: {
+    title: "Raqlin | Move for a Purpose",
+    description: "Budget Friendly Tour Packages For Indian Middle Class",
+    url: "https://raqlin.com",
+    siteName: "Raqlin",
+    images: [
+      {
+        url: "/logo-white.png",
+        width: 1200,
+        height: 630,
+        alt: "Raqlin Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -32,6 +49,16 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        {/* <!-- Clarity tracking code for https://raqlin.com/ --> */}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
     </html>
   );
